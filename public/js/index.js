6c4071854a019a958fa88a00a498b2d7e3cd5ca7
Michael Thompson <mroythompson@gmail.com>
2014-10-24 12:12:32 -0700

    Adding templates for the slots

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+$(function() {
+  var game = Slots;
+  game.start()
+});
--- public/js/slots.js
+++ public/js/slots.js
@@ -1,3 +1,42 @@
-$(function() {
-  console.log('hi');
-});
+Slots = (function() {
+  var slots = {}
+
+  slots.start = function() {
+    setupView();
+  }
+
+  function setupView() {
+    setupTemplates();
+    var template = Handlebars.compile(Templates.slots);
+    var slotsView = template(getSlotsData());
+    $('#slot-columns').html(slotsView);
+    bindEvents();
+  }
+
+  function setupTemplates() {
+    Templates = {};
+    Templates.slots = $('#slots-template').html();
+    Templates._reel = $('#reel-template').html();
+    Templates._box = $('#box-template').html();
+
+    Handlebars.registerPartial('_reel', Templates._reel);
+    Handlebars.registerPartial('_box', Templates._box);
+  }
+
+  function bindEvents() {
+    $('#spin-button').click(function(event) {
+      event.preventDefault();
+      handleSpin();
+    });
+  }
+
+  function handleSpin() {
+    console.log('yo');
+  }
+
+  function getSlotsData() {
+    return [[0,1,2,0,1,2], [0,1,2,0,1,2], [0,1,2,0,1,2]];
+  }
+
+  return slots;
+})();
