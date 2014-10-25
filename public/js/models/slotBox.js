d1dec1803ede111587cd93bbe786fd7588d55297
Michael Thompson <mroythompson@gmail.com>
2014-10-24 20:59:44 -0700

    Adding SlotBox model for templates

---
+++ public/js/models/slotBox.js
@@ -0,0 +1,13 @@
+// 0 = coffee
+// 1 = espresso
+// 2 = tea
+
+SlotBox = (function() {
+
+  function SlotBox(val, category) {
+    this.val = val;
+    this.category = category;
+  }
+
+  return SlotBox
+})();
